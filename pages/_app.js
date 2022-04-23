ba9762a83ede7477e1a97f3c56b27c42ea0204ba
Simple Sigma <trashalphonse@gmail.com>
2022-04-23 21:38:05 +0300

    remove animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,10 @@
 import '../styles/globals.css'
 import Layout from '../comps/Layout'
-import { motion, AnimatePresence } from 'framer-motion'
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <AnimatePresence exitBeforeEnter initial={false}>
-        <motion.div className="page-animation" key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
-          pageAnimate: {
-            opacity: 1,
-            x: 0,
-          },
-          pageExit: {
-            opacity: 0,
-            x: 100,
-          },
-          transition: {
-            type: "spring", 
-            stiffness: 100,
-            duration: .2
-          }
-        }}>
-            <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
+        <Component {...pageProps} />
     </Layout>
   )
 }
